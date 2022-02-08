39a105cad972a5a0f9603e6473af45c2491358d7
4ndersom <andersodderupmadsen@hotmail.com>
2022-02-08 16:23:31 +0100

    Add files via upload

---
+++ src/app/Catalouge/pokemon-catalogue.page.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemonCataloguePage } from './pokemon-catalogue.page';
+
+describe('PokemonCataloguePage', () => {
+  let component: PokemonCataloguePage;
+  let fixture: ComponentFixture<PokemonCataloguePage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PokemonCataloguePage ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PokemonCataloguePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
