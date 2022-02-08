5704b6f79825d10671424bde57f27a6eba72ad1b
4ndersom <andersodderupmadsen@hotmail.com>
2022-02-08 16:22:24 +0100

    Add files via upload

---
+++ src/app/Catalouge/pokemon-catalogue-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-catalogue-item',
+  templateUrl: './pokemon-catalogue-item.component.html',
+  styleUrls: ['./pokemon-catalogue-item.component.css']
+})
+export class PokemonCatalogueItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
